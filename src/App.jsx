6b81cc94c20d6021bd06bb870b53ff4e9abe1007
vishal dutt sharma <vishalduttsharma5@gmail.com>
2026-04-18 01:09:50 +0530

    project created 18-4-2026

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Footer from './component/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
